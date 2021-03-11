cbd8f3a5245c5198e1e04b9b26bd39db8afb45f5
Dohyeon Han <limited.hyeon@gmail.com>
2021-03-11 23:26:32 +0900

    slide

--- move.js
+++ move.js
@@ -1,15 +1,31 @@
-const ul = document.getElementsByClassName("slideList"),
+const right = document.getElementById("btnRight"),
+    left = document.getElementById("btnLeft"),
+    ul = document.getElementsByClassName("slideList"),
     li = Array.from(document.getElementsByClassName("contentsSlide"));
     radio = document.getElementsByName("slide");
+    
+const whatSlide = ["date","todoList","weather"];
+var pos = 1;
 
+function addBtnEvent(){
+    right.addEventListener("click",function(event){
+        pos = (pos+1)%3;
+        radio[pos].checked = true;
+        changeSlide(whatSlide[pos]);
+    })
+    left.addEventListener("click",function(event){
+        pos=(pos-1<0?2:--pos);
+        radio[pos].checked = true;
+        changeSlide(whatSlide[pos]);
+    })
+}
 
 function changeSlide(item){
-    console.log(li);
-    if(item.id==="date"){
+    if(item==="date"){
         li.forEach(function(element){
             element.style.transform=`translateX(100%)`;
         });
-    }else if(item.id==="todoList"){
+    }else if(item==="todoList"){
         li.forEach(function(element){
             element.style.transform=`translateX(0%)`;
         });
@@ -23,18 +39,15 @@ function changeSlide(item){
 function getChangedRadio(){
     radio.forEach(element => {
         element.addEventListener("click", function(event) {
-            var item = event.target;
+            var item = event.target.id;
             changeSlide(item);
           });
     });
 }
-function getCheckedRadio(){
-
-}
 
 function init(){
-    getCheckedRadio();
     getChangedRadio();
+    addBtnEvent();
 }
 
 init();
