b7d8b61aade02e783cd748739c04227f6bbc7f69
Dohyeon Han <limited.hyeon@gmail.com>
2021-03-11 23:26:50 +0900

    date

---
+++ today.js
@@ -0,0 +1,29 @@
+const time = document.getElementById("time"),
+    cal = document.getElementById("date");
+
+const dayList = ["Sunday","Monday"
+,"Tuseday","Wednesday","Thursday","Friday","Saturday"]
+
+function nowTime(){
+    const today = new Date();
+    const hours = today.getHours();
+    const minutes = today.getMinutes();
+    const seconds = today.getSeconds();
+    time.innerText = `${hours<10?`0${hours}`:`${hours}`}:`+
+                    `${minutes<10?`0${minutes}`:`${minutes}`}:`+
+                    `${seconds<10?`0${seconds}`:`${seconds}`}`;
+    const day = today.getDay();
+    const month = today.getMonth();
+    const date = today.getDate();
+    cal.innerText = `${month<10?`0${month}`:`${month}`}/`+
+                    `${date<10?`0${date}`:`${date}`}
+                    ${dayList[day]}`
+
+}
+
+function init(){
+    nowTime();
+    setInterval(nowTime,1000);
+}
+
+init();
